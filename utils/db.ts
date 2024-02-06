36d08bb2ffa94887df2c6003784235c235a3a41f
devMorshed <ac3m0r@gmail.com>
2024-02-06 23:24:15 +0600

    databse connection setup

---
+++ utils/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+require("dotenv").config();
+
+const dbUrl: string = process.env.DB_URI || "";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(dbUrl).then((data: any) => {
+      console.log(`Database is connected with ${data.connection.host}`);
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    setTimeout(connectDB, 500);
+  }
+};
