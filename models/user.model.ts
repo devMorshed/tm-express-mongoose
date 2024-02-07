e56b5adcae9b93489f1f15b5bdbb57daee88b1a0
devMorshed <ac3m0r@gmail.com>
2024-02-07 17:37:11 +0600

    User model.

--- models/user.model.ts
+++ models/user.model.ts
@@ -1,73 +1,78 @@
 require("dotenv").config();
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import mongoose, { Document, Model, Schema } from "mongoose";
 
 const emailRegexPattern: RegExp =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
 
+// Creating IUser interface by extending Document
 export interface IUser extends Document {
   name: string;
+  batch: number;
+  room: number;
+  balance: number;
+  avatar: string;
+  role: string;
   email: string;
   password: string;
-  avatar: {
-    public_id: string;
-    url: string;
-  };
-  role: string;
   isVerified: boolean;
-  courses: Array<{ courseId: string }>;
+  isBlocked: boolean;
   comparePassword: (password: string) => Promise<boolean>;
-  signAccessToken: () => string;
-  signRefreshToken: () => string;
 }
 
-const userSchema: Schema<IUser> = new mongoose.Schema(
+// User Schema based on IUser interface.
+const UserSchema: Schema<IUser> = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Please enter your name"],
+      required: [true, "Please enter your full name"],
+    },
+    batch: {
+      type: Number,
+      required: [true, "Please enter your batch number"],
+    },
+    room: {
+      type: Number,
+      required: [true, "Please enter your batch number"],
     },
     email: {
       type: String,
-      required: [true, "Please enter your email"],
+      required: [true, "Please enter your email adress"],
       validate: {
         validator: function (value: string) {
           return emailRegexPattern.test(value);
         },
-        message: "Please enter a valid email",
+        message: "Please enter a valid email address",
       },
       unique: true,
     },
-
     password: {
       type: String,
-      // required: [true, "Please provide your password"],
-      minlength: [6, "Password must be atleast 6 charecters"],
+      minlength: [6, "Password can't be shorter than 6 characters. "],
       select: false,
     },
     avatar: {
-      public_id: String,
-      url: String,
+      type: String,
+      default: "",
     },
     role: {
       type: String,
-      default: "User",
+      default: "USER",
     },
     isVerified: {
       type: Boolean,
       default: false,
     },
-    courses: [
-      {
-        courseId: String,
-      },
-    ],
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
 
-userSchema.pre<IUser>("save", async function (next) {
+// Hasshing Password
+UserSchema.pre<IUser>("save", async function (next) {
   if (!this.isModified("password")) {
     next();
   }
@@ -75,26 +80,13 @@ userSchema.pre<IUser>("save", async function (next) {
   next();
 });
 
-// Sign Access Token here.
-userSchema.methods.signAccessToken = function () {
-  return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "", {
-    expiresIn: "5m",
-  });
-};
-
-// Sign Refresh Token here
-userSchema.methods.signRefreshToken = function () {
-  return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN || "", {
-    expiresIn: "3d",
-  });
-};
-
-userSchema.methods.comparePassword = async function (
-  enteredPassword: string
+// Comparing password
+UserSchema.methods.comparePassword = async function (
+  enteredPass: string
 ): Promise<boolean> {
-  return await bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPass, this.password);
 };
 
-const userModel: Model<IUser> = mongoose.model("User", userSchema);
+const userModel: Model<IUser> = mongoose.model("User", UserSchema);
 
 export default userModel;
