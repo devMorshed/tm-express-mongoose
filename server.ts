d3a7c585082ee189c7d6c2fb672af7b124d9ee51
devMorshed <ac3m0r@gmail.com>
2024-02-18 11:03:38 +0600

    connecting db before server listening

--- server.ts
+++ server.ts
@@ -2,8 +2,13 @@ import app from "./app";
 import { connectDB } from "./utils/db";
 require("dotenv").config();
 
-// Server creation
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-  connectDB();
-});
+// Server creation after connecting database
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
